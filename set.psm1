7d6eafbc5777c051565fafe279d83662841eeb4f
Caglar Yalcin <github@caglaryalcin.com>
2024-08-25 15:38:52 +0300

    Update set.psm1

--- set.psm1
+++ set.psm1
@@ -183,7 +183,7 @@ Function UnusedApps {
         }
         
         # homepath additional settings
-        New-Item -Path $homePath2 -Force
+        New-Item -Path $homePath2 -Force *>$null
         Set-ItemProperty -Path $homePath2 -Name "(Default)" -Value "CLSID_MSGraphHomeFolder"
         Set-ItemProperty -Path $homePath2 -Name "HiddenByDefault" -Value 1 -Type DWord
 
